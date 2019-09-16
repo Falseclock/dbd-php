d8b5341582a0dd38e58f36d51499e6abfb6930c0
Nurlan Mukhanov <102@mp.kz>
2019-09-16 21:20:51 +0600

    remove template comment

--- src/Base/Query.php
+++ src/Base/Query.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * <description should be written here>
- *
- * @package      DBD\Base
- * @copyright    Copyright © Real Time Engineering, LLP - All Rights Reserved
- * @license      Proprietary and confidential
- * Unauthorized copying or using of this file, via any medium is strictly prohibited.
- * Content can not be copied and/or distributed without the express permission of Real Time Engineering, LLP
- *
- * @author       Written by Nurlan Mukhanov <nmukhanov@mp.kz>, сентябрь 2019
- */
-
-namespace DBD\Base;
 
 namespace DBD\Base;
 
