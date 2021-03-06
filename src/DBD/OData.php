414966a9503345198757f021d733f9c1bf79f5fd
Nurlan Mukhanov <102@mp.kz>
2021-03-06 13:44:23 +0600

    ODATA eq, ne, lt, le, ge, gt

--- src/DBD/OData.php
+++ src/DBD/OData.php
@@ -233,7 +233,7 @@ class OData extends DBD
                 $where
             );
 
-            $where = str_replace(['=', '>', '<'], ['eq', 'ge', 'le'], $where);
+            $where = str_replace(['=', '<>', '>=', '<=', '>', '<'], ['eq', 'ne', 'ge', 'le', 'gt', 'lt'], $where);
             $where = str_replace(array_keys($params), array_values($params), $where);
 
             $this->requestUrl .= '$filter=' . $where . '&';
