64804ecf1eca8ec9c181ba1008183acbb8e3e158
Nurlan Mukhanov <102@mp.kz>
2021-03-20 12:20:17 +0600

    v2.0.0-RC1

---
+++ tests/Odata/OdataUnsupportedTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * OdataUnsupportedTest
+ *
+ * @author    Nurlan Mukhanov <nurike@gmail.com>
+ * @copyright 2020 Nurlan Mukhanov
+ * @license   https://en.wikipedia.org/wiki/MIT_License MIT License
+ * @link      https://github.com/Falseclock/dbd-php
+ */
+
+declare(strict_types=1);
+
+namespace DBD\Tests\Odata;
+
+use DBD\Common\DBDException;
+
+class OdataUnsupportedTest extends OdataTest
+{
+    /**
+     * @throws DBDException
+     * @covers \DBD\Odata::_begin
+     */
+    public function testBegin()
+    {
+        self::expectException(DBDException::class);
+        $this->db->begin();
+    }
+
+    /**
+     * @throws DBDException
+     * @covers \DBD\Odata::_commit
+     */
+    public function testCommit()
+    {
+        self::expectException(DBDException::class);
+        $this->db->commit();
+    }
+
+    /**
+     * @throws DBDException
+     * @covers \DBD\Odata::_rollback
+     */
+    public function testRollback()
+    {
+        self::expectException(DBDException::class);
+        $this->db->rollback();
+    }
+}
